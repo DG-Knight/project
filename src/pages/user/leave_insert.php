79059d472eec6a0d654898f0eea5a1b52229546a
Apiwat Dindang <sao.apiwat@gmail.com>
2018-12-23 16:44:28 +0700

    insert leave

--- src/pages/user/leave_insert.php
+++ src/pages/user/leave_insert.php
@@ -1,6 +1,6 @@
 <?php
 
-//$user_id = $_POST['user_id'];
+$user_id = $_SESSION['AUTHEN']['UID'];
 $leave_type_id = $_POST['leave_type_id'];
 $leave_start = $_POST['leave_start'];
 $leave_end = $_POST['leave_end'];
@@ -8,20 +8,19 @@ $total = $_POST['total'];
 
 
   $conn = PDOConnector();
-  $result = $conn->prepare('INSERT INTO leaves(leave_type_id, leave_start, leave_end, total)VALUES(:leave_type_id, :leave_start, :leave_end, :total)');
-  $result ->execute([
+  $result = $conn->prepare( 'INSERT INTO leaves (user_id, leave_type_id, leave_start, leave_end, total) VALUES (:user_id, :leave_type_id, :leave_start, :leave_end, :total)');
 
-    //"user_id"=>$user_id,
-    "leave_type_id"=>$leave_type_id,
-    "leave_start"=>$leave_start,
-    "leave_end"=>$leave_end,
-    "total"=>$total
-
-  ]);
+    $result ->execute([
+      "user_id"=>$user_id,
+      "leave_type_id"=>$leave_type_id,
+      "leave_start"=>$leave_start,
+      "leave_end"=>$leave_end,
+      "total"=>$total
+    ]);
 if ($result) {
   echo "<script>
       alert('จัดการข้อมูลสำเร็จแล้ว');
-
+      window.location = 'dashboard.php?file=user/leave';
     </script>";
 }else {
 echo "error";
