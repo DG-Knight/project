e6bc0248a4a2f55f884fb8b762feb29434aa82cf
DG-Knight <sao.apiwat@gmail.com>
2019-02-12 10:28:31 +0700

    chang_word

--- src/pages/admin/nurse/change_ward_form.php
+++ src/pages/admin/nurse/change_ward_form.php
@@ -10,41 +10,13 @@
           <div class="form-row">
             <div class="form-group col-md-5">
               <label for="person_chang">พยาบาลที่ขอเปลี่ยนเวร</label>
-              <select id="person_chang" name="person_chang" class="form-control">
-                <option></option>
-
-                <?php
-                $conn = PDOConnector();
-                $sql = 'SELECT * FROM users where user_level != 0';
-                $query = $conn->prepare($sql);
-                $query->execute();
-                   if ($query->rowCount()>0) {
-                      $i = 1;
-                      while ($data = $query -> fetch(PDO::FETCH_OBJ)) {
-                ?>
-                <option value="<?=$data->user_id;?>"><?=$data->user_fname;?>&nbsp<?=$data->user_lname;?></option>
-
-              <?php }}//EndRowCount&&WhileLoop ?>
-              </select>
+              <input type="text" id="person_chang" name="person_chang" class="form-control">
             </div>
+
             <div class="form-group col-md-5">
               <label for="nurse_chang">พยาบาลที่เปลี่ยนเวร</label>
-              <select id="nurse_chang" name="nurse_chang" class="form-control">
-                <option></option>
+              <input type="text"id="nurse_chang" name="nurse_chang" class="form-control">
 
-                <?php
-                $conn = PDOConnector();
-                $sql = 'SELECT * FROM users where user_level != 0';
-                $query = $conn->prepare($sql);
-                $query->execute();
-                   if ($query->rowCount()>0) {
-                      $i = 1;
-                      while ($data = $query -> fetch(PDO::FETCH_OBJ)) {
-                ?>
-                <option value="<?=$data->user_id;?>"><?=$data->user_fname;?>&nbsp<?=$data->user_lname;?></option>
-
-              <?php }}//EndRowCount&&WhileLoop ?>
-              </select>
             </div>
 
             <div class="form-group col-md-5">
@@ -56,27 +28,18 @@
             <label for="opt_names">เวรที่ปลี่ยน</label>
             <select id="opt_names" name="opt_names" class="form-control">
               <option></option>
-
-              <?php
-              $conn = PDOConnector();
-              $sql = 'SELECT * FROM operating_time';
-              $query = $conn->prepare($sql);
-              $query->execute();
-                 if ($query->rowCount()>0) {
-                    $i = 1;
-                    while ($data = $query -> fetch(PDO::FETCH_OBJ)) {
-              ?>
-              <option value="<?=$data->opt_time_id;?>"><?=$data->opt_name;?></option>
-
-            <?php }}//EndRowCount&&WhileLoop ?>
+              <option>เช้า</option>
+              <option>บ่าย</option>
+              <option>ดึก</option>
             </select>
           </div>
         </div>
-          <button type="submit" class="btn btn-primary">submit</button>
-          <button type="reset" class="btn btn-primary">reset</button>
+        
+        <button type="submit" class="btn btn-primary">submit</button>
+        <button type="reset" class="btn btn-primary">reset</button>
+
         </form>
 
-      </div>
 </div><!--end card-->
 </div><!--end card-body-->
 </div><!--end col-md-6 grid-margin stretch-card-->
